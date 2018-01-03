9954ca93f1be5dad7ef8472179cab8fe6bea6118
Mark Fitzpatrick <markfitz815@gmail.com>
2018-01-03 16:43:36 +0000

    Logo added and default width created

--- scripts/header.js
+++ scripts/header.js
@@ -1,5 +1,20 @@
+const site = document.getElementById('site');
+const head = document.getElementById('head');
+
+function createHead(){
+    console.log('heading');
+    const headDiv = document.createElement('div');
+    headDiv.id="siteHead";
+    head.appendChild(headDiv);
+
+    const headLogo = document.createElement('div');
+    headLogo.id = "siteHeadLogo";
+    headLogo.innerHTML="MF";
+    headDiv.appendChild(headLogo);
+
+}
 
 
 (function initHeader(){
-    console.log('here');
+    createHead()
 })()
