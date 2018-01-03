4c55281b3f683af068e3ed12cce3fedcfcc5bde1
Mark Fitzpatrick <markfitz815@gmail.com>
2018-01-03 17:38:36 +0000

    layout of header complete

--- scripts/header.js
+++ scripts/header.js
@@ -1,17 +1,31 @@
 const site = document.getElementById('site');
 const head = document.getElementById('head');
+const linkList = ['Home', 'Portfolio', 'About', 'Contact']
 
 function createHead(){
-    console.log('heading');
+    //Header Container
     const headDiv = document.createElement('div');
     headDiv.id="siteHead";
     head.appendChild(headDiv);
-
+    //Head Logo
     const headLogo = document.createElement('div');
     headLogo.id = "siteHeadLogo";
     headLogo.innerHTML="MF";
     headDiv.appendChild(headLogo);
+    //Head Links
+    const headLinks = document.createElement('ul');
+    headLinks.id="siteHeadLinks";
 
+    for(var i = 0; i < linkList.length; i++){
+        const tempList = document.createElement('li');
+        if(i == 1){
+            tempList.classList += 'siteHeadLinksCurrent';
+        }
+        tempList.innerHTML = linkList[i];
+        headLinks.appendChild(tempList);
+    }
+
+    headDiv.appendChild(headLinks);
 }
 
 
