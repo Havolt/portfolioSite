eff8115104c6d56f63dda4a97231e2f175bd58c1
Mark Fitzpatrick <markfitz815@gmail.com>
2018-01-05 17:24:17 +0000

    Fixed links margin causing error with main section. Added a short bio to the index page

--- scripts/indexScript.js
+++ scripts/indexScript.js
@@ -1,3 +1,14 @@
-const tmpP = document.createElement('p')
-tmpP.innerHTML='This is just a test';
-main.appendChild(tmpP)
+
+function homeBio(){
+    console.log('zoom zoom');
+    const bioText = "Hi, I'm Mark. I'm an Irish based front end web developer. I am skilled in HTML, CSS, JavaScript."
+
+    const bioDiv = document.createElement('div');
+    bioDiv.innerHTML= bioText;
+    bioDiv.classList.add('bioDiv')
+    main.appendChild(bioDiv);
+}
+
+(function initMain(){
+    homeBio();
+})()
