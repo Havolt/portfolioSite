403ead9ff0a35c5d2924127edaedc08aa256f3d5
Mark Fitzpatrick <markfitz815@gmail.com>
2018-01-11 18:32:12 +0000

    link maker function created and title styled and given onclick

--- ver2/scripts/createEl.js
+++ ver2/scripts/createEl.js
@@ -1,4 +1,4 @@
-function creEl(elType, elClass, appendee, innHTML, srce){
+function creEl(elType, elClass, appendee, innHTML, srce,){
     const newSec = document.createElement(elType);
     if(typeof(elClass) == 'string'){newSec.classList.add(elClass);}
     else{for(var i = 0; i < elClass.length; i++){newSec.classList.add(elClass[i])};}
@@ -6,4 +6,11 @@ function creEl(elType, elClass, appendee, innHTML, srce){
     if(innHTML){newSec.innerHTML= innHTML;}
     if(srce){newSec.src = srce;}
     appendee.appendChild(newSec);
+}
+
+function opnPg(targetEl, location, onSelf){
+    targetEl.addEventListener('click', function(){
+        if(onSelf){winOp = '_self'}else{winOp = '_blank'}
+        window.open(location, winOp);
+    })
 }
--- ver2/scripts/headerScript.js
+++ ver2/scripts/headerScript.js
@@ -1,6 +1,6 @@
 
 const headContainObj = {type: 'div', class: 'headContainer', apnd: head};
-const headTitleObj = {type: 'div', class: 'headTitle', apnd: "document.getElementsByClassName('headContainer')[0]", inHL : "MF" };
+const headTitleObj = {type: 'div', class: 'headTitle', apnd: "document.getElementsByClassName('headContainer')[0]", inHL : "MF", hrf: 'index.html', onSelf: true };
 
 function buildHead(ob){
     creEl(ob.type, ob.class, ob.apnd);
@@ -8,6 +8,7 @@ function buildHead(ob){
 
 function buildHeadTitle(ob){
     creEl(ob.type, ob.class, eval(ob.apnd), ob.inHL)
+    opnPg(eval('document.getElementsByClassName(ob.class)[0]'), ob.hrf, ob.onSelf);
 }
 
 
