ead49423ba0becf9ed9979e02a32985786076bfa
Mark Fitzpatrick <markfitz815@gmail.com>
2018-01-15 20:51:14 +0000

    Styling added to icons at 1024px and window.open event added to each of them to correct website

--- ver2/scripts/footerScript.js
+++ ver2/scripts/footerScript.js
@@ -2,13 +2,15 @@ let footIconData = [];
 const iconContainData = {type: 'div', class: 'footIconContain', append: document.getElementById("foot")}
 
 //Creates data for icon links
-function fIconDataConstruct(cl, inHL){
+function fIconDataConstruct(cl, inHL, lnk){
     let tmpObjF = {};
     tmpObjF.type = 'div';
     tmpObjF.class = ['footIcon'];
     tmpObjF.class.push(cl);
     tmpObjF.append = eval('document.getElementsByClassName("footIconContain")[0]');
     tmpObjF.inHL = inHL;
+    tmpObjF.num = footIconData.length;
+    tmpObjF.link = lnk;
     footIconData.push(tmpObjF);
 }
 
@@ -20,13 +22,14 @@ function createFootElement(ob){
 function createFootIcons(arr){
     for(let i = 0; i < arr.length; i++){
         creEl(arr[i].type, arr[i].class, arr[i].append, arr[i].inHL);
+        opnPg(document.getElementsByClassName("footIcon")[i], arr[i].link, false);
     }
 }
 
 (function initFooter(){
     createFootElement(iconContainData);
-    fIconDataConstruct('gitIcon', '<i class="fa fa-github"></i>');
-    fIconDataConstruct('twitterIcon', '<i class="fa fa-twitter"></i>');
-    fIconDataConstruct('facebookIcon', '<i class="fa fa-facebook"></i>');
+    fIconDataConstruct('gitIcon', '<i class="fa fa-github"></i>', 'https://github.com/Havolt');
+    fIconDataConstruct('twitterIcon', '<i class="fa fa-twitter"></i>', 'https://twitter.com/jetsetfitz');
+    fIconDataConstruct('facebookIcon', '<i class="fa fa-facebook"></i>', 'https://www.facebook.com/mark.fitzpatrick.545');
     createFootIcons(footIconData);
 })()
