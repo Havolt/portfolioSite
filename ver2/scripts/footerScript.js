a9ca51ea7f7881b13817e2b23e8523b424785e05
Mark Fitzpatrick <markfitz815@gmail.com>
2018-01-16 13:59:15 +0000

    Links added to footer with working hyperlinks

--- ver2/scripts/footerScript.js
+++ ver2/scripts/footerScript.js
@@ -1,5 +1,6 @@
 let footIconData = [];
-const iconContainData = {type: 'div', class: 'footIconContain', append: document.getElementById("foot")}
+const iconContainData = {type: 'div', class: 'footIconContain', append: document.getElementById("foot")};
+let footLinkData = [];
 
 //Creates data for icon links
 function fIconDataConstruct(cl, inHL, lnk){
@@ -26,10 +27,39 @@ function createFootIcons(arr){
     }
 }
 
+function createFootLinkData(arr){
+    for(let i = 0; i < sitePages.length; i++){
+        let tmpOb = {};
+        tmpOb.type = 'li';
+        tmpOb.inHL = sitePages[i];
+        if(currPage == sitePages[i]){
+            tmpOb.class = ['footLinkLi', 'footLinkLiCurrent'];
+        }else{tmpOb.class = 'footLinkLi'}
+        arr.push(tmpOb);
+    }
+}
+
+function createFootLinks(arr){
+    creEl('ul', 'footLinkUl', foot)
+    for(let i = 0; i < arr.length; i++){
+        creEl(arr[i].type, arr[i].class, document.getElementsByClassName('footLinkUl')[0], arr[i].inHL);
+        if(typeof(arr[i].class) == 'string'){
+            if(sitePages[i] == 'Home'){opnPg(document.getElementsByClassName(arr[i].class)[i], 'index.html', true)}
+            else{opnPg(document.getElementsByClassName(arr[i].class)[i], sitePages[i].toLowerCase()+'.html', true)}
+        }else{
+            if(sitePages[i] == 'Home'){opnPg(document.getElementsByClassName(arr[i].class[0])[i], 'index.html', true)}
+            else{opnPg(document.getElementsByClassName(arr[i].class[0])[i], sitePages[i].toLowerCase()+'.html', true)}
+        }
+    }
+}
+
 (function initFooter(){
     createFootElement(iconContainData);
     fIconDataConstruct('gitIcon', '<i class="fa fa-github"></i>', 'https://github.com/Havolt');
     fIconDataConstruct('twitterIcon', '<i class="fa fa-twitter"></i>', 'https://twitter.com/jetsetfitz');
     fIconDataConstruct('facebookIcon', '<i class="fa fa-facebook"></i>', 'https://www.facebook.com/mark.fitzpatrick.545');
     createFootIcons(footIconData);
+    createFootLinkData(footLinkData);
+    console.log(footLinkData)
+    createFootLinks(footLinkData);
 })()
