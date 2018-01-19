129d5d08c3a5160f38a76c42a2be06fa0227e2f6
Mark Fitzpatrick <markfitz815@gmail.com>
2018-01-19 19:05:51 +0000

    Added background picture to contact page and divs for elements on top of img

--- ver2/scripts/contactScript.js
+++ ver2/scripts/contactScript.js
@@ -1,1 +1,19 @@
 const currPage = sitePages[3];
+
+const contactMainData = [{type: 'div', class: 'contactImgContainer', apnd: 'contactContainer'},
+                        {type: 'img', class: 'contactMainImg', apnd: 'contactImgContainer', inHL: 'undefined', src: 'images/contact/rope-island.jpg'},
+                        {type: 'div', class: 'contactMainTitle', apnd: 'contactImgContainer', inHL: 'Get in touch'},
+                        {type: 'div', class: 'contactMainText', apnd: 'contactImgContainer', inHL: 'I am currently looking for new opportunities of employment. If you have a position you feel I would be a good fit for or have an inquiry please send me an email below:'},
+                        {type: 'div', class: 'contactMainButton', apnd: 'contactImgContainer', inHL: 'Send an Email'}]
+
+function createContactSection(arr){
+    console.log(arr.length)
+    for(let i = 0; i < arr.length; i++){
+        creEl(arr[i].type, arr[i].class, document.getElementsByClassName(arr[i].apnd)[0], arr[i].inHL, arr[i].src)
+    }
+}
+
+(function initContact(){
+    creEl('div', 'contactContainer', main)
+    createContactSection(contactMainData);
+})()
